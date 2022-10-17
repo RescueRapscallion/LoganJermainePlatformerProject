263ef29ff3c568e74798f19cae63622c57a7b57a
RescueRapscallion <105831892+RescueRapscallion@users.noreply.github.com>
2022-10-17 18:48:04 -0500

    up1
    aughhhh

--- Assets/code/PlayerBeh.cs
+++ Assets/code/PlayerBeh.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class PlayerBeh : MonoBehaviour
 {
-    public float speed = 10;
+    public float Speed = 10;
     public GameObject Blast;
     // public Vector3 BlastPos;
-    public Vector2 Force = new Vector2(0, 20);
+    public Vector2 Force = new Vector2(0, 50);
     public bool OnGround;
     Rigidbody2D rb2d;
-    public Vector2 jumpForce = new Vector2(0, 300);
-
+    public Vector2 JumpForce = new Vector2(0, 5000);
+    
     public bool beenHit = false;
-
+    public bool Look = true;
     public AudioClip TapSound;
     public AudioClip CollisionSound;
 
@@ -30,31 +30,65 @@ public class PlayerBeh : MonoBehaviour
         // make it move 
         float move = Input.GetAxis("Horizontal");
         Vector3 pos = transform.position;
-        pos.x += move * speed * Time.deltaTime;
-       // pos.x = Mathf.Clamp(pos.x, -20.5f, 20.5f);
+        pos.x += move * Speed * Time.deltaTime;
+        // pos.x = Mathf.Clamp(pos.x, -20.5f, 20.5f);
         transform.position = pos;
         bool shouldJump = (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.DownArrow));
-        if (shouldJump && !beenHit && OnGround)
+       // if (shouldJump && !beenHit && OnGround)
         {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(jumpForce);
+
+          //  rb2d.velocity = Vector2.zero;
+           // rb2d.AddForce(JumpForce);
 
         }
         //if (Input.GetKeyDown(KeyCode.Space))
         {
-         //   rb2d.AddForce(Vector2.up * force, (ForceMode2D)ForceMode.Impulse);
+            //   rb2d.AddForce(Vector2.up * force, (ForceMode2D)ForceMode.Impulse);
         }
         //BlastPos = new Vector3(transform.position.x + 3, transform.position.y, transform.position.z);
 
         {
 
         }
-       // if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.Space))
         {
-           // Instantiate(Blast, BlastPos, Quaternion.identity);
+            // Instantiate(Blast, BlastPos, Quaternion.identity);
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
 
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+        if (GetComponent<SpriteRenderer>().flipX == true)
+        {
+            // player facing left
+            if (shouldJump && !beenHit && OnGround)
+            {
+
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(JumpForce);
+
+            }
+
+        }
+        else
+        {
+            //player facing right
+            if (shouldJump && !beenHit && OnGround)
+            {
+
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(JumpForce );
+                rb2d.AddForce(transform.right*JumpForce);
+            }
+        }
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
