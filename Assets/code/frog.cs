8e89fc688461eab0fe4b5934d552799eb8b0cb6c
Redhead210 <jermaine.jaxson@gmail.com>
2022-11-04 16:09:40 -0500

    le2
    frog

--- Assets/code/frog.cs
+++ Assets/code/frog.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class frog : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool Frog;
     void Start()
     {
         
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Frog"))
+        {
+            Application.Quit();
+        }
+    }
     // Update is called once per frame
     void Update()
     {
