e7611d71b599e9b437ffe30ce3ce5d43c3c75833
RescueRapscallion <jermaine.jaxson@gmail.com>
2022-11-10 15:36:51 -0600

    PI3
    broken update fix soon

---
+++ Assets/code/DEmen.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DEmen : MonoBehaviour
+{
+    public void Start()
+    {
+        Application.targetFrameRate = 120;
+
+
+    }
+
+    public void LScene0()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LScene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LScene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LScene3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LScene4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LScene5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void LScene6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Scene7()
+    {
+        SceneManager.LoadScene(7);
+    }
+}
